b8be7836d5ab042479b5f083ae330e318cdb888c
tangjm <jaredtang@hotmail.co.uk>
2021-12-14 14:36:42 +0000

    Added test for Header component

---
+++ frontend/src/tests/Header.test.js
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+
+import Header from '../Components/Header';
+
+describe(`Header component tests`, () => {
+	beforeEach(() => {
+		render(<Header />);
+
+	})
+
+	test(`it should render the df logo`, () => {
+		const element = screen.getByRole("img");
+		expect(element).toBeInTheDocument();
+	})
+
+	xtest(`it should render a "need help?" button`, () => {
+		const element = screen.getByText(/need help?/i);
+
+		expect(element).toBeInTheDocument();
+	})
+	xtest(`it should render a "talk to us" button`, () => {
+
+		const element = screen.getByText(/need help?/i);
+
+		expect(element).toBeInTheDocument();
+	})
+	xtest(`it should render a "menu" button`, () => {
+
+		const element = screen.getByText(/need help?/i);
+
+		expect(element).toBeInTheDocument();
+	})
+
+})
