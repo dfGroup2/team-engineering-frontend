9e079a0d0f15960c4d13690ae6830078daaca9bc
Anthony Sarsfield <sarsfielda@outlook.com>
2021-12-14 15:46:05 +0000

    passed user name test

---
+++ frontend/src/Components/ProfileInfo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ProfileInfo = props => {
+    return (
+        <div>
+            <p>Name:</p>
+
+        </div>
+    )
+}
+
+ProfileInfo.propTypes = {
+
+}
+
+export default ProfileInfo
