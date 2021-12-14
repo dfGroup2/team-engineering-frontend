41a1800f80eb251aa8f1d5b8f040b61a973b1385
tangjm <jaredtang@hotmail.co.uk>
2021-12-14 14:37:03 +0000

    Added Header component; test passes for Header component

---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = props => {
+	return (
+		<header>
+			<img src="../images/logo.svg" alt="df_logo" />
+		</header>
+	)
+}
+
+Header.propTypes = {
+
+}
+
+export default Header;
