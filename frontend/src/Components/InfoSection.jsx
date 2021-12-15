e1684749a861b861fb244674a15dcc7cac1c93bb
Sara-Gad <saragadhs@hotmail.com>
2021-12-15 15:37:25 +0000

    completed the test

---
+++ frontend/src/Components/InfoSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const InfoSection = props => {
+    return (
+        <div className="container card borderClass">
+           
+        </div>
+    )
+}
+
+InfoSection.propTypes = {
+
+}
+
+export default InfoSection
