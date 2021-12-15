c49f8fa3f8eac2674c63314d9d57ff7f960e56a5
KatieStocker <stocker_katie@aol.co.uk>
2021-12-15 11:36:19 +0000

    added code to pass the test

---
+++ frontend/src/Components/TrainingSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const TrainingSection = props => {
+    return (
+        <div>
+            <h3>Your training</h3>
+        </div>
+    )
+}
+
+TrainingSection.propTypes = {
+
+}
+
+export default TrainingSection
