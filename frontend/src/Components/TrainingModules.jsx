6224d63071fad5148df57606085153c4773f9f3b
KatieStocker <stocker_katie@aol.co.uk>
2021-12-15 13:22:21 +0000

    added styling

--- frontend/src/Components/TrainingModules.jsx
+++ frontend/src/Components/TrainingModules.jsx
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types'
 
 const TrainingModules = props => {
     return (
-        <div>
-            <p>Module 1, Challenge A: PASSED</p>
-            <p>Module 1, Challenge B: PASSED</p>
-            <p>Module 2, Challenge C: TODO</p>
+        <div className="row">
+            <div className="col-4">
+                <p>Module 1, Challenge A: PASSED</p>
+                <p>Module 1, Challenge B: PASSED</p>
+                <p>Module 2, Challenge C: TODO</p>
+            </div>
         </div>
     )
 }
