6e53c77ee8c9db3deb6665f1d6fa7ba3caeb2647
Matt Gibbons <m_gibbons@hotmail.co.uk>
2021-12-15 10:15:12 +0000

    added PersonalStory component and test

---
+++ frontend/src/Components/PersonalStory.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const PersonalStory = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+PersonalStory.propTypes = {
+
+}
+
+export default PersonalStory
