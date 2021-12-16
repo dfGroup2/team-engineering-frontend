c5d2cedc9f8b7f5c36b7e8cdc09a2797532e9f7e
KatieStocker <stocker_katie@aol.co.uk>
2021-12-16 14:04:13 +0000

    added some styling to the EditPersonalInfoSection

--- frontend/src/Components/EditProfileComponents/EditPersonalInfoSection.jsx
+++ frontend/src/Components/EditProfileComponents/EditPersonalInfoSection.jsx
@@ -3,34 +3,39 @@ import PropTypes from 'prop-types'
 
 const EditPersonalInfoSection = props => {
     return (
-        <div>
-            <h3>Personal Information</h3>
+        <div className="container card borderClass">
+            <h3 className="col-3">Personal Information</h3>
             <div className="row">
                 <div className="col-6">
                     <form action="">
-                        <div>
-                            <label htmlFor="Name">Name</label>
-                            <input type="text" name="Name" />
+                        <div className="col-6">
+                            <div>
+                                <label htmlFor="Name" className="col-6">Name</label>
+                                <input type="text" name="Name" className="col-6" />
+                            </div>
+                            <div>
+                                <label htmlFor="p-email" className="col-6">Personal Email</label>
+                                <input type="email" name="p-email" className="col-6" />
+                            </div>
+                            <div>
+                                <label htmlFor="df-email" className="col-6">Digital Futures Email</label>
+                                <input type="email" name="df-email" className="col-6" />
+                            </div>
+                            <div>
+                                <label htmlFor="GitHub" className="col-6">GitHub</label>
+                                <input type="text" name="GitHub" className="col-6" />
+                            </div>
+                            <div>
+                                <label htmlFor="linkedIn" className="col-6">LinkedIn</label>
+                                <input type="text" name="linkedIn" className="col-6" />
+                            </div>
+                            <div>
+                                <label htmlFor="phone" className="col-6">Phone</label>
+                                <input type="number" name="phone" className="col-6" />
+                            </div>
                         </div>
-                        <div>
-                            <label htmlFor="p-email">Personal Email</label>
-                            <input type="email" name="p-email" />
-                        </div>
-                        <div>
-                            <label htmlFor="df-email">Digital Futures Email</label>
-                            <input type="email" name="df-email" />
-                        </div>
-                        <div>
-                            <label htmlFor="GitHub">GitHub</label>
-                            <input type="text" name="GitHub" />
-                        </div>
-                        <div>
-                            <label htmlFor="linkedIn">LinkedIn</label>
-                            <input type="text" name="linkedIn" />
-                        </div>
-                        <div>
-                            <label htmlFor="phone">Phone</label>
-                            <input type="number" name="phone" />
+                        <div className="col-6">
+
                         </div>
                     </form>
                 </div>
