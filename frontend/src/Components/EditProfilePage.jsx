55ffd5f8267530e329a7484fb2da68e38009df24
Sara-Gad <saragadhs@hotmail.com>
2021-12-16 11:27:24 +0000

    passing test

---
+++ frontend/src/Components/EditProfilePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
+
+const EditProfilePage = props => {
+	return (
+		<div className="container borderClass">
+			<div className="row">
+				<h3 className="col-2">Your profile</h3>
+				<br />
+				<br />
+			</div>
+			</div>
+	)
+}
+
+EditProfilePage.propTypes = {
+
+}
+
+export default EditProfilePage;
