5eba8698fb0e5b04d4f92fc56f72e7d3f670fdca
Matt Gibbons <m_gibbons@hotmail.co.uk>
2021-12-16 14:56:56 +0000

    added failed test

---
+++ frontend/src/tests/EditPersonalStory.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import EditPersonalStory from '../Components/EditProfileComponents/EditPersonalStory';
+
+describe(`Tests for EditPersonalStory`, () => {
+	beforeEach(() => {
+		render(
+			<Router>
+				<EditPersonalStory />
+			</Router>
+		);
+	});
+
+	test(`it should render a "personal story" heading`, () => {
+		const element = screen.getByText(/personal story/i);
+		expect(element).toBeInTheDocument();
+	});
+
+
+});
