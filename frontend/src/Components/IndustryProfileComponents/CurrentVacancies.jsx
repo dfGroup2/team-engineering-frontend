49167180eb3109262b4c41a4287c21be6eaa3ab7
Sara-Gad <saragadhs@hotmail.com>
2021-12-17 13:55:01 +0000

    passing test

--- frontend/src/Components/IndustryProfileComponents/CurrentVacancies.jsx
+++ frontend/src/Components/IndustryProfileComponents/CurrentVacancies.jsx
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types'
 const CurrentVacancies = props => {
 	return (
 		<div className="container borderClass">
+            <h3>Current vacancies</h3>
 			
 		</div>
 	)
