71920137f1ced517be9a2d7ef6ae7406a2622016
Sara-Gad <saragadhs@hotmail.com>
2021-12-17 11:48:14 +0000

    passing test

---
+++ frontend/src/Components/IndustryProfileComponents/IndustryProfilePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+const IndustryProfilePage = props => {
+	return (
+		<div className="container borderClass">
+			<h3>your industry profile</h3>
+		</div>
+	)
+}
+
+IndustryProfilePage.propTypes = {
+
+}
+
+export default IndustryProfilePage;
