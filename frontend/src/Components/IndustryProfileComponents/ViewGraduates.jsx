24a2f5c9adc27ad0d15409672fa1f829cb1e10f7
Sara-Gad <saragadhs@hotmail.com>
2021-12-17 13:31:18 +0000

    passing test

---
+++ frontend/src/Components/IndustryProfileComponents/ViewGraduates.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ViewGraduates = props => {
+	return (
+		<div className="container borderClass">
+			<h3>graduates</h3>
+            <img alt="graduate image"></img>
+            <img alt="graduate image"></img>
+            <img alt="graduate image"></img>
+        
+		</div>
+	)
+}
+
+ViewGraduates.propTypes = {
+
+}
+
+export default ViewGraduates;
