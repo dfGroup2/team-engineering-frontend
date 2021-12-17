6498ea966cad07fc9b8eff65380147299132cb6e
Sara-Gad <saragadhs@hotmail.com>
2021-12-17 13:53:06 +0000

    failing test

---
+++ frontend/src/Components/IndustryProfileComponents/CurrentVacancies.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+const CurrentVacancies = props => {
+	return (
+		<div className="container borderClass">
+			
+		</div>
+	)
+}
+
+CurrentVacancies.propTypes = {
+
+}
+
+export default CurrentVacancies;
