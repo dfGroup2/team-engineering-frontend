14f8a243e18b1911c536eda1745a63faf3cea6b0
KatieStocker <stocker_katie@aol.co.uk>
2021-12-20 13:30:05 +0000

    added some test data for us to use with testing props

---
+++ frontend/src/tests/testData/sampleGraduateUser.js
@@ -0,0 +1,118 @@
+const testGraduateUser = {
+    graduateProfile:
+    {
+        firstName: "Bilbo",
+        lastName: "Baggins",
+        personalEmail: "bbaggins@theshire.com",
+        DFEmail: "bb1@digitalfutures.com",
+        github: "https://github.com/",
+        linkedIn: "https://www.linkedin.com/",
+        phoneNumber: "01234567",
+        profilePicture: "me.png",
+        personalStory: "The Hobbit",
+        gender: "Male",
+        nationality: "New Zealand",
+        personality: "ESFJ - Consul"
+    },
+    graduateTraining:
+    {
+        learningPath: `Software Engineering`,
+        cohort: `Software Engineering 3`,
+        trainer: `Ed Wright`,
+        finishedDate: `Jan 11, 2022`,
+        courseModules:
+        {
+            fundamentals = {
+                "javascript programming": `pending`
+            },
+            "software design": {
+                "bank challenge": `pending`,
+                "news summary challenge": `pending`,
+                "rock paper scissors challenge": `pending`,
+                "chitter challenge": `pending`,
+                "test-driven development": `pending`,
+            },
+            "software development 1": {
+                "3rd party integration": `pending`,
+            },
+            "software development 2": {
+                "business services": `pending`,
+                "full-stack application": `pending`,
+            },
+            "team engineering": {
+                "java challenge": `pending`,
+                "agile team collaboration": `pending`
+            },
+            "professional skills": {
+                "interview challenges": `pending`
+            }
+        }
+    },
+    personalInfo:
+    {
+        badges: [`Badge A`, `Badge B`, `Badge C`],
+        scores: [`XX`, `XX`, `XX`],
+        videoLink: ``
+    },
+    personalStory:
+    {
+        degree:
+        {
+            university: `UOL`,
+            subject: `Comp Sci`,
+            level: `BSc`,
+            grade: `P`,
+            date: { from: ``, to: `` },
+            weight: `L`,
+            priority: 1,
+            description: `university degree`
+        },
+        schoolQualifications:
+        {
+            school: `high school`,
+            examType: `GCSE`,
+            subject: `IT`,
+            grade: `Dist`,
+            year: { from: `2011`, to: `2012` },
+            weight: `m`,
+            priority: 1,
+            description: `high school qualification`
+        },
+        certificatesAndAwards:
+        {
+            type: ``,
+            issuer: ``,
+            award: ``,
+            grade: ``,
+            year: ``,
+            weight: ``,
+            priority: 1,
+            description: ``
+        },
+        portfolio:
+        {
+            title: ``,
+            url: ``,
+            year: ``,
+            weight: ``,
+            priority: 1,
+            description: ``
+        },
+        workExperience:
+        {
+            type: ``,
+            employerOrOtherOrganisation: ``,
+            position: ``,
+            date: { from: ``, to: `` },
+            weight: ``,
+            priority: 1,
+            description: ``
+        }
+    },
+    news:
+    {
+
+    }
+}
+
+module.exports = { testGraduateUser };
---
+++ frontend/src/tests/testData/samplePersonalStoryData.js
@@ -0,0 +1,58 @@
+const testPersonalStory = {
+    degree:
+    {
+        university: `UOL`,
+        subject: `Comp Sci`,
+        level: `BSc`,
+        grade: `P`,
+        date: { from: ``, to: `` },
+        weight: `L`,
+        priority: 1,
+        description: `university degree`
+    },
+    schoolQualifications:
+    {
+        school: `high school`,
+        examType: ``,
+        subject: ``,
+        grade: ``,
+        year: { from: ``, to: `` },
+        weight: ``,
+        priority: 1,
+        description: `high school qualification`
+    },
+    certificatesAndAwards:
+    {
+        type: ``,
+        issuer: ``,
+        award: ``,
+        grade: ``,
+        year: ``,
+        weight: ``,
+        priority: 1,
+        description: ``
+    },
+    portfolio:
+    {
+        title: ``,
+        url: ``,
+        year: ``,
+        weight: ``,
+        priority: 1,
+        description: ``
+    },
+    workExperience:
+    {
+        type: ``,
+        employerOrOtherOrganisation: ``,
+        position: ``,
+        date: { from: ``, to: `` },
+        weight: ``,
+        priority: 1,
+        description: ``
+    }
+};
+
+module.exports = {
+    testPersonalStory
+};
