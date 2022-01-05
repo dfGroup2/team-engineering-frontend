390571386343fc5d1a7f07217bdb759925e0a86a
KatieStocker <stocker_katie@aol.co.uk>
2022-01-05 16:36:37 +0000

    added failing tests for training section props

--- src/tests/TrainingSection.test.js
+++ src/tests/TrainingSection.test.js
@@ -78,10 +78,52 @@ describe(`Test Suite for Training Section`, () => {
             expect(screen.getByText(/chitter challenge/i)).toBeInTheDocument();
         });
 
+        test(`it should render testDrivenDevelopment`, () => {
+            expect(screen.getByText(/test driven challenge/i)).toBeInTheDocument();
+        });
+
+        test(`it should render thirdPartyIntegration`, () => {
+            expect(screen.getByText(/third Party Integration/i)).toBeInTheDocument();
+        });
+
+        test(`it should render businessServices`, () => {
+            expect(screen.getByText(/business Services/i)).toBeInTheDocument();
+        });
+
+        test(`it should render fullStackApplication`, () => {
+            expect(screen.getByText(/full Stack Application/i)).toBeInTheDocument();
+        });
+
+        test(`it should render javaChallenge`, () => {
+            expect(screen.getByText(/java Challenge/i)).toBeInTheDocument();
+        });
+
+        test(`it should render agileTeamCollaboration`, () => {
+            expect(screen.getByText(/agile Team Collaboration/i)).toBeInTheDocument();
+        });
+
+        test(`it should render interviewChallenges`, () => {
+            expect(screen.getByText(/interview Challenges/i)).toBeInTheDocument();
+        });
 
         xtest(`it should render "pending" 9 times`, () => {
             const numberPending = screen.getAllByText(/pending/i).length;
             expect(numberPending).toBe(9);
+        });
+
+        test(`it should render pass 1 time`, () => {
+            const numberPass = screen.getAllByText(/pass/i).length;
+            expect(numberPass).toBe(1);
+        });
+
+        test(`it should render failed 1 time`, () => {
+            const numberFailed = screen.getAllByText(/Fail/i).length;
+            expect(numberFailed).toBe(1);
+        });
+
+        test(`it should render todo 1 time`, () => {
+            const numberTodo = screen.getAllByText(/todo/i).length;
+            expect(numberTodo).toBe(1);
         });
 
     });
