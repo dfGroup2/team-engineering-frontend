20f3249d7163750145b258e2b521a9e35313292e
KatieStocker <stocker_katie@aol.co.uk>
2022-01-07 11:57:04 +0000

    one test was failing since we updated the signup/login pages, I have now amended this test

--- src/tests/testData/SignUpPage.test.js
+++ src/tests/testData/SignUpPage.test.js
@@ -14,42 +14,45 @@ describe(`Tests for SignUp Page`, () => {
 	test(`it should render a "Digital Futures" heading`, () => {
 		const element = screen.getByText(/digital futures/i);
 		expect(element).toBeInTheDocument();
-    });
-    test(`it should render signup for an account`, () => {
+	});
+
+	test(`it should render signup for an account`, () => {
 		const element = screen.getByText(/signup for an account/i);
 		expect(element).toBeInTheDocument();
-    });
-    test(`it should render Email`, () => {
+	});
+
+	test(`it should render Email`, () => {
 		const element = screen.getByText(/Email/i);
 		expect(element).toBeInTheDocument();
-    });
-    test(`it should render First name`, () => {
+	});
+
+	test(`it should render First name`, () => {
 		const element = screen.getByText(/First name/i);
 		expect(element).toBeInTheDocument();
-    });
-     test(`it should render Last name`, () => {
+	});
+
+	test(`it should render Last name`, () => {
 		const element = screen.getByText(/Last name/i);
 		expect(element).toBeInTheDocument();
-     });
-     test(`it should render Company`, () => {
+	});
+
+	test(`it should render Company`, () => {
 		const element = screen.getByText(/Company/i);
 		expect(element).toBeInTheDocument();
-     });
-    test(`it should render SignUp button`, () => {
+	});
+
+	test(`it should render SignUp button`, () => {
 		const element = screen.getByDisplayValue(/SignUp/i);
 		expect(element).toBeInTheDocument();
-    });
-    test(`it should render a background image`, () => {
-		const {container} = render(
+	});
+
+	test(`it should render an image`, () => {
+		const { container } = render(
 			<Router>
 				<SignUp />
 			</Router>
-        )
-        const element = container.querySelector('.background-image')
+		)
+		const element = container.querySelector('.dfximage')
 		expect(element).toBeTruthy();
-    });
-
-    
-
-
+	});
 });
