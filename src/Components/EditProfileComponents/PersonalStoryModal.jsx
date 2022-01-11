dea2b3d6308bb98d71df83cca1e0a11e466f7a78
Matt Gibbons <m_gibbons@hotmail.co.uk>
2022-01-11 10:36:47 +0000

    changing information on each Modal

--- src/Components/EditProfileComponents/PersonalStoryModal.jsx
+++ src/Components/EditProfileComponents/PersonalStoryModal.jsx
@@ -53,12 +53,182 @@ const PersonalStoryModal = ({ show, setShowModal, inputFieldHeaders }) => {
     const handleSubmit = () => {
 
     }
+    const renderDegrees = () => {
+        return (<form action="">
+                                <div className="">
+                                    <div className="form-inputs">
+                                        <label htmlFor="Name" className="col-6">University</label>
+                                        <input type="text" value={university} name="university" className="col-6" onChange={handleUniversityChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="p-email" className="col-6">DegreeSubject</label>
+                                        <input type="email" value={degreeSubject} name="degreeSubject" className="col-6" onChange={handleDegreeSubjectChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="df-email" className="col-6">Degree Level</label>
+                                        <input type="email" value={degreeLevel} name="degreeLevel" className="col-6" p onChange={handleDegreeLevelChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="GitHub" className="col-6">Grade</label>
+                                        <input type="text" value={grade} name="grade" className="col-6" onChange={handleGradeChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="linkedIn" className="col-6">From</label>
+                                        <input type="text" value={from} name="from" className="col-6" onChange={handleFromChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="phone" className="col-6">To</label>
+                                        <input type="number" value={to} name="to" className="col-6" onChange={handleToChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="phone" className="col-6">Description</label>
+                                        <input type="text" value={description} name="description" className="col-6" onChange={handleDescriptionChange} />
+                                     </div>
+                                </div>
+                            </form>)
+    }
+
+    const renderSchoolQuals = () => {
+        return (<form action="">
+                                <div className="">
+                                    <div className="form-inputs">
+                                        <label htmlFor="Name" className="col-6">School</label>
+                                        <input type="text" value={school} name="school" className="col-6" onChange={handleSchoolChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="p-email" className="col-6">Exam Type</label>
+                                        <input type="text" value={examType} name="examType" className="col-6" onChange={handleDegreeExamType} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="df-email" className="col-6">Subject</label>
+                                        <input type="text" value={subject} name="subject" className="col-6" p onChange={handleSubject} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="GitHub" className="col-6">Grade</label>
+                                        <input type="text" value={grade} name="grade" className="col-6" onChange={handleGradeChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="phone" className="col-6">Year</label>
+                                        <input type="date" value={year} name="year" className="col-6" onChange={handleYearChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="phone" className="col-6">Description</label>
+                                        <input type="text" value={description} name="description" className="col-6" onChange={handleDescriptionChange} />
+                                     </div>
+                                </div>
+                            </form>)
+    }
+
+    const renderWorkExperience = () => {
+        return(<form action="">
+                                <div className="">
+                                    <div className="form-inputs">
+                                        <label htmlFor="Name" className="col-6"></label>
+                                        <input type="text" value={university} name="university" className="col-6" onChange={handleUniversityChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="p-email" className="col-6">DegreeSubject</label>
+                                        <input type="email" value={degreeSubject} name="degreeSubject" className="col-6" onChange={handleDegreeSubjectChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="df-email" className="col-6">Degree Level</label>
+                                        <input type="email" value={degreeLevel} name="degreeLevel" className="col-6" p onChange={handleDegreeLevelChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="GitHub" className="col-6">Grade</label>
+                                        <input type="text" value={grade} name="grade" className="col-6" onChange={handleGradeChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="linkedIn" className="col-6">From</label>
+                                        <input type="text" value={from} name="from" className="col-6" onChange={handleFromChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="phone" className="col-6">To</label>
+                                        <input type="number" value={to} name="to" className="col-6" onChange={handleToChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="phone" className="col-6">Description</label>
+                                        <input type="text" value={description} name="description" className="col-6" onChange={handleDescriptionChange} />
+                                     </div>
+                                </div>
+                            </form>)
+    }
+
+    const renderCertificates = () => {
+        return(<form action="">
+                                <div className="">
+                                    <div className="form-inputs">
+                                        <label htmlFor="Name" className="col-6">University</label>
+                                        <input type="text" value={university} name="university" className="col-6" onChange={handleUniversityChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="p-email" className="col-6">DegreeSubject</label>
+                                        <input type="email" value={degreeSubject} name="degreeSubject" className="col-6" onChange={handleDegreeSubjectChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="df-email" className="col-6">Degree Level</label>
+                                        <input type="email" value={degreeLevel} name="degreeLevel" className="col-6" p onChange={handleDegreeLevelChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="GitHub" className="col-6">Grade</label>
+                                        <input type="text" value={grade} name="grade" className="col-6" onChange={handleGradeChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="linkedIn" className="col-6">From</label>
+                                        <input type="text" value={from} name="from" className="col-6" onChange={handleFromChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="phone" className="col-6">To</label>
+                                        <input type="number" value={to} name="to" className="col-6" onChange={handleToChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="phone" className="col-6">Description</label>
+                                        <input type="text" value={description} name="description" className="col-6" onChange={handleDescriptionChange} />
+                                     </div>
+                                </div>
+                            </form>)
+    }
+
+    const renderPortfolio = () => {
+        return(<form action="">
+                                <div className="">
+                                    <div className="form-inputs">
+                                        <label htmlFor="Name" className="col-6">University</label>
+                                        <input type="text" value={university} name="university" className="col-6" onChange={handleUniversityChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="p-email" className="col-6">DegreeSubject</label>
+                                        <input type="email" value={degreeSubject} name="degreeSubject" className="col-6" onChange={handleDegreeSubjectChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="df-email" className="col-6">Degree Level</label>
+                                        <input type="email" value={degreeLevel} name="degreeLevel" className="col-6" p onChange={handleDegreeLevelChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="GitHub" className="col-6">Grade</label>
+                                        <input type="text" value={grade} name="grade" className="col-6" onChange={handleGradeChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="linkedIn" className="col-6">From</label>
+                                        <input type="text" value={from} name="from" className="col-6" onChange={handleFromChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="phone" className="col-6">To</label>
+                                        <input type="number" value={to} name="to" className="col-6" onChange={handleToChange} />
+                                    </div>
+                                    <div className="form-inputs">
+                                        <label htmlFor="phone" className="col-6">Description</label>
+                                        <input type="text" value={description} name="description" className="col-6" onChange={handleDescriptionChange} />
+                                     </div>
+                                </div>
+                            </form>)
+    }
 
     return (
 
         <Modal show={show} onHide={handleClose}>
             <Modal.Header >
-                <Modal.Title>Degree</Modal.Title>
+                <Modal.Title>Degrees:</Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 <div className="container card borderClass">
